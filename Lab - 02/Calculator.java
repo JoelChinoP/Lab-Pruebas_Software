88dc57106f95fbcbd11266a23d291ae17759b074
José Luis Cusilayme García <jcusilaymeg@unsa.edu.pe>
2025-05-15 16:23:52 -0500

    Creando Espacio de Trabajo Lab 02

---
+++ Lab - 02/Calculator.java
@@ -0,0 +1,11 @@
+/**
+ * Clase simple que ofrece operaciones básicas de suma y resta.
+ */
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+}
